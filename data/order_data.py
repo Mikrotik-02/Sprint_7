15afb331a94662955dec60e3dc8ae58f97d75821
Mikrotik-02 <kovalartem20020172@gmail.com>
2026-05-31 16:59:44 +0300

    Add create order tests

--- data/order_data.py
+++ data/order_data.py
@@ -1,1 +1,24 @@
-# Здесь будут тестовые данные заказа.
+class OrderData:
+    BASE_ORDER_PAYLOAD = {
+        "firstName": "Ivan",
+        "lastName": "Ivanov",
+        "address": "Moscow, Test street, 1",
+        "metroStation": 4,
+        "phone": "+79991234567",
+        "rentTime": 3,
+        "deliveryDate": "2026-06-06",
+        "comment": "Test order"
+    }
+
+    ORDER_COLORS = [
+        ["BLACK"],
+        ["GREY"],
+        ["BLACK", "GREY"],
+        []
+    ]
+
+    @staticmethod
+    def get_order_payload_with_color(color):
+        payload = OrderData.BASE_ORDER_PAYLOAD.copy()
+        payload["color"] = color
+        return payload
--- tests/test_create_order.py
+++ tests/test_create_order.py
@@ -1,1 +1,19 @@
-# Здесь будут тесты создания заказа.
+import pytest
+
+from api.order_api import OrderAPI
+from data.order_data import OrderData
+
+
+class TestCreateOrder:
+    def setup_method(self):
+        self.order_api = OrderAPI()
+
+    @pytest.mark.parametrize("color", OrderData.ORDER_COLORS)
+    def test_create_order_with_different_colors_returns_track(self, color):
+        payload = OrderData.get_order_payload_with_color(color)
+
+        response = self.order_api.create_order(payload)
+
+        assert response.status_code == 201
+        assert "track" in response.json()
+        assert response.json()["track"]
