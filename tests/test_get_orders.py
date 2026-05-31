a096fb14c56f502c27cd0e1636b893450c49391a
Mikrotik-02 <kovalartem20020172@gmail.com>
2026-05-31 17:07:16 +0300

    Add get orders test

--- tests/test_get_orders.py
+++ tests/test_get_orders.py
@@ -1,1 +1,13 @@
-# Здесь будут тесты получения списка заказов.
+from api.order_api import OrderAPI
+
+
+class TestGetOrders:
+    def setup_method(self):
+        self.order_api = OrderAPI()
+
+    def test_get_orders_returns_orders_list(self):
+        response = self.order_api.get_orders()
+
+        assert response.status_code == 200
+        assert "orders" in response.json()
+        assert isinstance(response.json()["orders"], list)
